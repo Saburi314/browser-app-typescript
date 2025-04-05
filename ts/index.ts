c9aaab86b253e07e49e6b7cd740d3bdbf8cbed92
Saburi <nnta20yy@docomo.ne.jp>
2025-04-05 11:52:10 +0900

    Application と EventListenerの実装

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,24 @@
-import { sum } from "./sum"
+import { EventListener } from "./Eventlistener"
 
-console.log("Hello World Again!!");
-console.log(sum(1, 2));
+class Application {
+    start() {
+    const eventListener = new EventListener()
+    const button = document.getElementById('deleteAllDoneTask')
+
+    if (!button) return
+
+    eventListener.add(
+        'sample',
+        'click',
+        button,
+        () => alert('clicked'),
+    )
+
+    eventListener.remove('sample')
+    }   
+}
+
+const app = new Application();
+app.start();
+
+
