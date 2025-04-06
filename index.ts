08a87b851779bbe695ee71ac6dcb00e3dc2dc25f
Saburi <nnta20yy@docomo.ne.jp>
2025-04-06 11:45:31 +0900

    index.ts関連

--- index.ts
+++
@@ -1,9 +0,0 @@
-start() {
-    const createForm = document.getElementById('createForm') as HTMLFormElement
-    this.eventListener.add(
-        'summit-handler',
-        'submit',
-        createForm,
-        this.handleSubmit.bind(this),  // bindを使用してthisを固定
-    )
-} 
